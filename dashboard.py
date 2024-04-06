338190fce85ea1ce36ab892c1bb2f48152caacd5
Madisen Arurang <marurang3@gatech.edu>
2024-04-06 12:35:06 -0700

    update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -28,6 +28,7 @@ df = pd.DataFrame(data)
 
 # Create app
 app = dash.Dash(__name__, external_stylesheets=['https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'])
+server = app.server
 
 # Define app layout
 app.layout = html.Div([
